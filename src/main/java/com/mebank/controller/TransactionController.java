3576470660e09f8991134e525d564da295c13083
markondavineeth25 <vineeth.markonda@gmail.com>
2020-02-17 12:12:48 +1100

    updated returnValue

--- src/main/java/com/mebank/controller/TransactionController.java
+++ src/main/java/com/mebank/controller/TransactionController.java
@@ -1,16 +1,16 @@
 package com.mebank.controller;
 
-import com.mebank.service.TransactionService;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.ParseException;
+import java.util.HashMap;
+import com.mebank.service.TransactionService;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 @Controller
 public class TransactionController {
@@ -22,15 +22,15 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
-    public String listTransactions() throws IOException, ParseException {
+    public HashMap<String, Object> listTransactions() throws IOException, ParseException {
         System.out.println( "Controller: list transactions" );
 
         Reader reader = Files.newBufferedReader(Paths.get("transactions.csv"));
         CSVParser csvParser = new CSVParser(reader,
                 CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());
 
-        transactionService.processTransactions(csvParser);
+        HashMap<String, Object> outputMap = transactionService.processTransactions(csvParser);
 
-        return "books";
+        return outputMap;
     }
 }
